a39940b0c5803a9e773cdb3d6f1d60dcf4a24023
Mikel Bishop <mdrew8@gmail.com>
2020-09-04 23:06:03 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+  
+  def initialize(title)
+    @title = title
+  end 
+  
+  attr_reader :title
+  
+  attr_accessor :genre, :page_count, :author 
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end 
+  
+end 
 
-
+# https://learn.co/tracks/online-software-engineering-structured/object-oriented-ruby/section-5-object-lifecycle/oo-basics#https://learn.co/tracks/online-software-engineering-structured/object-oriented-ruby/section-5-object-lifecycle/oo-basics#
